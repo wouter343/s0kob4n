8742e00adde4e55f89138fcbaf822254f2d78d7d
unknown <wwjthies@avans.nl>
2018-10-05 16:13:51 +0200

    all fixed

--- Sokoban/Tile.cs
+++ Sokoban/Tile.cs
@@ -12,75 +12,27 @@ namespace Sokoban
 
         }
       
-        public Crate Kist
+        public Crate Chest
         {
             get => default(Crate);
             set
             {
             }
         }
-
-<<<<<<< HEAD
         public Boolean HasCrate
         {
             get => default(Boolean);
-=======
-        public int HasCrate
-        {
-            get => default(int);
             set
             {
             }
         }
 
-        public Tile HasPlayer
-        {
-            get => default(Tile);
-            set
-            {
-            }
-        }
-
-        public Tile down
-        {
-            get => default(Tile);
-            set
-            {
-            }
-        }
-
-        public Tile up
-        {
-            get => default(Tile);
->>>>>>> 8fa2859936765b3602ba013e30a327e86ee85e17
-            set
-            {
-            }
-        }
-
-<<<<<<< HEAD
         public Boolean HasPlayer
         {
             get => default(Boolean);
-=======
-        public Tile left
-        {
-            get => default(Tile);
->>>>>>> 8fa2859936765b3602ba013e30a327e86ee85e17
             set
             {
             }
         }
-
-<<<<<<< HEAD
-=======
-        public Tile right
-        {
-            get => default(Tile);
-            set
-            {
-            }
-        }
->>>>>>> 8fa2859936765b3602ba013e30a327e86ee85e17
     }
 }
